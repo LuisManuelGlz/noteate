d5a1d21db8be788da02fcf476e970932145cf8eb
Luis Manuel González <mannyglzg@gmail.com>
2020-10-26 11:06:05 -0700

    Add sign up route
    Added sign up DTO

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,4 +8,8 @@ export class AuthController {
   signIn(@Body() signInDto: SignInDto): Token {
     return { tokenType: 'Bearer', accessToken: 'abc', expiresIn: 1 };
   }
+
+  signUp(@Body() signUp: SignInDto): Token {
+    return { tokenType: 'Bearer', accessToken: 'abc', expiresIn: 1 };
+  }
 }
