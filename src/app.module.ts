99ed9eec05135d071d6141d54ab4d942d6fc7a76
Luis Manuel González <mannyglzg@gmail.com>
2020-11-15 00:51:17 -0800

    Serve static files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,19 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AppController } from './app.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AppService } from './app.service';
 import { NotesModule } from './notes/notes.module';
 import { JwtMiddleware } from './middlewares/jwt.middleware';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
@@ -22,7 +26,6 @@ import { JwtMiddleware } from './middlewares/jwt.middleware';
     UsersModule,
     NotesModule,
   ],
-  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule implements NestModule {
