83335f7bc2beec350949b35c43d61495bf977d77
Luis Manuel González <mannyglzg@gmail.com>
2020-11-14 22:26:20 -0800

    Add navbar html

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
-
-
 
 @NgModule({
   declarations: [NavigationComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule],
+  exports: [NavigationComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
