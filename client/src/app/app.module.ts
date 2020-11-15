280041197aaf0f8608f6ad2f0e36ecd76ab8d29f
Luis Manuel González <mannyglzg@gmail.com>
2020-11-14 23:22:39 -0800

    Add home html
    Added background

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { RouterModule } from '@angular/router';
+import { HomeModule } from './modules/home/home.module';
+import { NotesModule } from './modules/notes/notes.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    HomeModule,
+    NotesModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
